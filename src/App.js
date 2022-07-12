01277a5c0a5234ec1703371913d7ab4ead70576b
Awais Amjed <awaisamjed171@gmail.com>
2022-07-12 14:00:33 +0500

    Fix eslint issues & add rule for state mutation

--- src/App.js
+++ src/App.js
@@ -10,7 +10,11 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <span>
           <span>Learn </span>
@@ -40,7 +44,8 @@ function App() {
           >
             Redux Toolkit
           </a>
-          ,<span> and </span>
+          ,
+          <span> and </span>
           <a
             className="App-link"
             href="https://react-redux.js.org/"
--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ root.render(
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
