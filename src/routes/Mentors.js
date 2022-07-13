958e21f4afaf853deecf6e92b31bd0031b2bcc63
Awais Amjed <awaisamjed171@gmail.com>
2022-07-13 18:45:59 +0500

    Add Directional Button

--- src/routes/Mentors.js
+++ src/routes/Mentors.js
@@ -1,9 +1,11 @@
 import Button from '../features/button/Button';
+import DirectionalButton from '../features/button/DirectionalButton';
 
 const Mentors = () => (
   <div>
     Mentors Page
     <Button onClick={() => {}} child="Meow" />
+    <DirectionalButton left onClick={() => {}} />
   </div>
 );
 
