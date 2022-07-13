c2ceac7a0a461a1a1f7a8d96ff858bdfbc764fac
Mostafa Ahangarha <ahangarha@riseup.net>
2022-07-13 17:38:24 +0300

    Remove extra content from mentor page

--- src/routes/Mentors.js
+++ src/routes/Mentors.js
@@ -1,11 +1,6 @@
-import Button from '../features/button/Button';
-import DirectionalButton from '../features/button/DirectionalButton';
-
 const Mentors = () => (
-  <div className="bg-lime-500">
+  <div>
     Mentors Page
-    <Button onClick={() => {}} child="Meow" isWhite />
-    <DirectionalButton left onClick={() => {}} />
   </div>
 );
 
