fa93745f45e80cbf81ee98739ae6f9893e8c7728
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-07-14 20:43:55 +0100

    Fetch mentor from API.

--- src/features/mentor-details/mentorDetailsAPI.js
+++ src/features/mentor-details/mentorDetailsAPI.js
@@ -1,7 +1,7 @@
 // A mock function to mimic making an async request for data
 import CONFIG from '../../config.json';
 
-const fetchMentorDetails = async (mentorID) => {
+export const fetchMentorDetails = async (mentorID) => {
   const response = await fetch(
     `${CONFIG.BASE_URL}/mentors/${mentorID}/topics`,
     {
@@ -16,4 +16,17 @@ const fetchMentorDetails = async (mentorID) => {
   return data;
 };
 
-export default fetchMentorDetails;
+export const fetchMentor = async (mentorID) => {
+  const response = await fetch(
+    `${CONFIG.BASE_URL}/mentors/${mentorID}`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${CONFIG.API_KEY}`,
+      },
+    },
+  );
+  const data = await response.json();
+  return data;
+};
