25c63154fa3435d14d0b464196bd4e4935095fdc
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-07-14 14:58:15 +0100

    Add the created reducer to the store.

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import mentorDetailsReducer from '../features/mentor-details/mentorDetailsSlice';
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
+    mentorDetails: mentorDetailsReducer,
   },
 });
 
