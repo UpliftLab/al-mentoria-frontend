fe5971c61171b403c20e72f36f80be9a25847dc8
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-07-14 14:58:35 +0100

    Use redux store to get the data in the mentor details page.

--- src/features/mentor-details/MentorDetailsPage.js
+++ src/features/mentor-details/MentorDetailsPage.js
@@ -1,3 +1,26 @@
-const MentorDetailsPage = () => <div>Mentor Details</div>;
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMentorDetailsAsync } from './mentorDetailsSlice';
+
+const MentorDetailsPage = () => {
+  const dispatch = useDispatch();
+  const { mentorDetails } = useSelector(
+    (state) => state.mentorDetails,
+  );
+  const { id } = useParams();
+  useEffect(() => {
+    dispatch(fetchMentorDetailsAsync(id));
+  }, []);
+  return (
+    <div>
+      <h1>
+        {console.log(mentorDetails)}
+        Mentor Details
+        {id}
+      </h1>
+    </div>
+  );
+};
 
 export default MentorDetailsPage;
