ed29fdcb6ff2083b7e6ab40f9d52168c7e8366b2
Awais Amjed <awaisamjed171@gmail.com>
2022-07-15 18:48:34 +0500

    Add title in add reservation page

--- src/routes/AddReservation.js
+++ src/routes/AddReservation.js
@@ -2,6 +2,9 @@ const AddMentor = () => (
   <div id="add-mentor-page">
     <div className="bg-cover absolute inset-0" style={{ backgroundImage: 'url(https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1356&h=668&fit=crop)' }} />
     <div className="bg-lime-500 opacity-90 absolute inset-0" />
+    <div className="flex flex-col justify-center items-center absolute inset-0 mx-10 gap-6">
+      <p className="text-2xl tracking-widest font-bold text-white border-white border-b-[1px] pb-6 px-4 text-center">BOOK A RESERVATION WITH MENTOR NAME</p>
+    </div>
   </div>
 );
 
