22a9d2437d0501f5e7d42ef8adb7ccf08164939d
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-07-15 14:10:19 +0100

    Add reservations reducer to the store.

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import reservationsReducer from '../features/reservations/reservationsSlice';
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
+    reservations: reservationsReducer,
   },
 });
 
