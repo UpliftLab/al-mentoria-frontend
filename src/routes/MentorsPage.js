d7edf22aa003d345a00ecccd7c65a9a4deeff1ee
Mostafa Ahangarha <ahangarha@riseup.net>
2022-07-15 15:53:05 +0300

    add page structure

--- src/routes/MentorsPage.js
+++ src/routes/MentorsPage.js
@@ -1,7 +1,16 @@
 const Mentors = () => (
-  <div>
-    Mentors Page
-  </div>
+  <section className="w-full h-full flex flex-col justify-center">
+    <h1 className="text-3xl font-bold uppercase text-black text-center">
+      Our Mentors
+    </h1>
+    <p className="text-center text-gray-400 font-bold">
+      Choose a mentor to see details and reserve an appointment
+    </p>
+    <hr className="mx-auto border-t-4 border-dotted w-32 my-8" />
+    <div className="bg-gray-200">
+      SLIDER
+    </div>
+  </section>
 );
 
 export default Mentors;
