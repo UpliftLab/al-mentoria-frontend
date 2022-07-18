6ac4ebaa91389a676709f67aa68a72dfc21b5dfb
Awais Amjed <awaisamjed171@gmail.com>
2022-07-18 15:43:49 +0500

    Remove props and passed mentor through useLocation

--- src/routes/AddReservation.js
+++ src/routes/AddReservation.js
@@ -1,36 +1,35 @@
-import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
 import Button from '../features/button/Button';
 import DropDownButton from '../features/button/DropDownButton';
 
-const AddReservation = ({
-  mentor,
-}) => (
-  <div id="add-mentor-page">
-    <div className="bg-cover absolute inset-0" style={{ backgroundImage: 'url(https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1356&h=668&fit=crop)' }} />
-    <div className="bg-lime-500 opacity-90 absolute inset-0" />
-    <div className="flex flex-col justify-center items-center absolute inset-0 mx-10 gap-6">
-      <p className="text-2xl tracking-widest font-bold text-white border-white border-b-[1px] pb-6 px-4 text-center">BOOK A RESERVATION WITH MENTOR NAME</p>
-      <p className="text-white text-center max-w-3xl">
-        It is a long established fact that a reader will be distracted by the readable
-        content of a page when looking at its layout. The point of using Lorem Ipsum is that
-        it has a more-or-less normal distribution of letters, as opposed to using making it
-        look like readable English.
-      </p>
-      <div className="flex mt-4 gap-6">
-        <DropDownButton options={['topics', 'here']} onChange={() => { }} />
-        <Button onClick={() => {}} child="Book Now" isWhite />
+const AddReservation = () => {
+  const { state } = useLocation();
+  let mentor = null;
+  if (state) {
+    mentor = state.mentor;
+  }
+
+  console.log(mentor);
+
+  return (
+    <div id="add-mentor-page">
+      <div className="bg-cover absolute inset-0" style={{ backgroundImage: 'url(https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1356&h=668&fit=crop)' }} />
+      <div className="bg-lime-500 opacity-90 absolute inset-0" />
+      <div className="flex flex-col justify-center items-center absolute inset-0 mx-10 gap-6">
+        <p className="text-2xl tracking-widest font-bold text-white border-white border-b-[1px] pb-6 px-4 text-center">BOOK A RESERVATION WITH MENTOR NAME</p>
+        <p className="text-white text-center max-w-3xl">
+          It is a long established fact that a reader will be distracted by the readable
+          content of a page when looking at its layout. The point of using Lorem Ipsum is that
+          it has a more-or-less normal distribution of letters, as opposed to using making it
+          look like readable English.
+        </p>
+        <div className="flex mt-4 gap-6">
+          <DropDownButton options={['topics', 'here']} onChange={() => { }} />
+          <Button onClick={() => {}} child="Book Now" isWhite />
+        </div>
       </div>
     </div>
-  </div>
-);
-
-AddReservation.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
-  mentor: PropTypes.object,
-};
-
-AddReservation.defaultProps = {
-  mentor: null,
+  );
 };
 
 export default AddReservation;
