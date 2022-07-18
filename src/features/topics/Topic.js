9c7fee57e34b0b095ddcc3f27173b7deb91368a4
sboursen <dev.sboursen@gmail.com>
2022-07-18 12:40:18 +0100

    Modify no topic icone placeholder image

--- src/features/topics/Topic.js
+++ src/features/topics/Topic.js
@@ -6,13 +6,12 @@ const Topic = ({ label, icon, onClick }) => {
   const handleError = (e) => {
     const image = e.currentTarget;
     image.src = Image;
-    image.className += ' h-8';
   };
 
   return (
     <button type="button" onClick={onClick} className="group relative flex cursor-pointer items-center justify-start bg-gray-100 rounded shadow-sm hover:bg-opacity-90 hover:backdrop-blur-xl py-2 px-4 w-fit gap-2">
       <div className="w-12 flex justify-center">
-        <img src={icon} className="max-h-[32px]" alt={`${label}'icon`} onError={handleError} />
+        <img src={icon} alt={`${label}'icon`} className="w-12 h-12 object-contain" onError={handleError} />
       </div>
       <h3>{label}</h3>
       <div className="absolute right-[1px] top-[1px] content-none text-gray-100 group-hover:text-red-500">
