86a527c147b34e9c87de0725a680b6fb416ed573
Awais Amjed <awaisamjed171@gmail.com>
2022-07-19 14:10:53 +0500

    Add handle submit on form submit

--- src/features/add-mentor/AddMentorPage.js
+++ src/features/add-mentor/AddMentorPage.js
@@ -1,6 +1,33 @@
+import { toast } from 'react-toastify';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import Button from '../button/Button';
+import { addMentorAsync } from './addMentorSlice';
 
 const AddMentorPage = () => {
+  const dispatch = useDispatch();
+  const { status: userStatus, token } = useSelector((state) => state.user);
+  const { status: addMentorStatus } = useSelector((state) => state.addMentor);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await dispatch(addMentorAsync({
+      name: e.target.elements.name.value,
+      photo: e.target.elements.photo.value,
+      bio: e.target.elements.bio.value,
+      token,
+    })).unwrap().then((response) => {
+      if (response) {
+        toast.success('Mentor Created Successfully');
+        navigate('/reservations');
+      } else {
+        toast.error('Error Creating Mentor, Try again later.');
+      }
+    });
+  };
+
   const inputClasses = 'shadow appearance-none border w-full rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline';
   const labelClasses = 'w-full md:w-auto';
   const spanClasses = 'block text-gray-700 text-sm font-bold mb-2';
@@ -9,6 +36,7 @@ const AddMentorPage = () => {
     <div id="add-mentor-page" className="relative min-h-full w-full flex justify-center items-center bg-lime-600 py-16 px-8">
       <form
         className="flex flex-col bg-white shadow-md rounded px-8 pt-6 pb-8 gap-6"
+        onSubmit={handleSubmit}
       >
         <div className="flex flex-wrap gap-6">
           <label htmlFor="name" className={labelClasses}>
