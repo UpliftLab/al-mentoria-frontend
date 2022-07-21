922a5ff85b36e29c8714414536a1352e9d4e742b
Mostafa Ahangarha <ahangarha@riseup.net>
2022-07-21 18:00:05 +0300

    Change Link to html ancher tag for external links

--- src/features/header/SocialLink.js
+++ src/features/header/SocialLink.js
@@ -1,16 +1,17 @@
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const SocialLink = ({
   to,
   icon,
 }) => (
-  <Link
-    to={to}
+  <a
+    href={to}
     className="inline-block p-1 hover:text-lime-600"
+    target="_blank"
+    rel="noopener noreferrer"
   >
     {icon}
-  </Link>
+  </a>
 );
 
 SocialLink.propTypes = {
