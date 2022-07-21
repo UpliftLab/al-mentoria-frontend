011bf8697bba14d7b023ff97430fbc4bf8744cff
Soufiane Boursen <dev.sboursen@gmail.com>
2022-07-21 13:59:34 +0100

    Test the Loading component

--- src/features/loading/loading.js
+++ src/features/loading/loading.js
@@ -1,5 +1,5 @@
 const Loading = () => (
-  <span className="h-6 w-6 block rounded-full border border-t-2 border-t-lime-500 animate-spin" />
+  <span data-testid="loading" className="h-6 w-6 block rounded-full border border-t-2 border-t-lime-500 animate-spin" />
 );
 
 export default Loading;
